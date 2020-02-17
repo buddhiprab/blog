ca18172bf6f6a11e5d436ce7d6037b52107148ad
Buddhi Nagahawatte <SGARNABN@rhc.msig.com>
2020-02-17 17:58:16 +0800

    get all posts

--- src/main/java/com/buddhi/blog/controllers/BlogController.java
+++ src/main/java/com/buddhi/blog/controllers/BlogController.java
@@ -1,13 +1,17 @@
 package com.buddhi.blog.controllers;
 
 import com.buddhi.blog.dto.PostDto;
+import com.buddhi.blog.models.Post;
 import com.buddhi.blog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static org.springframework.beans.BeanUtils.copyProperties;
 
@@ -20,5 +24,11 @@ public class BlogController {
     public ResponseEntity<Long> createPost(@RequestBody PostDto req){
         Long id = postService.createPost(req);
         return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @GetMapping("posts")
+    public ResponseEntity<List<PostDto>> getPosts(){
+        List<PostDto> postDtos = postService.getPosts();
+        return new ResponseEntity<>(postDtos,HttpStatus.OK);
     }
 }
--- src/main/java/com/buddhi/blog/services/PostService.java
+++ src/main/java/com/buddhi/blog/services/PostService.java
@@ -3,10 +3,13 @@ package com.buddhi.blog.services;
 import com.buddhi.blog.dto.PostDto;
 import com.buddhi.blog.models.Post;
 import com.buddhi.blog.repositories.PostRepository;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.springframework.beans.BeanUtils.copyProperties;
 
@@ -21,5 +24,16 @@ public class PostService {
         post.setCreationTime(new Date());
         Post postDb = postRepository.save(post);
         return postDb.getId();
+    }
+
+    public List<PostDto> getPosts() {
+        List<PostDto> postDtos = new ArrayList<>();
+        List<Post> posts = postRepository.findAll();
+        for(Post post:posts){
+            PostDto postDto = new PostDto();
+            copyProperties(post,postDto);
+            postDtos.add(postDto);
+        }
+        return postDtos;
     }
 }
